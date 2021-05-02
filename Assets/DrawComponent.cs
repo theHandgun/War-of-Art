9c6a296db9d8dc2cf08ca937d5e335babcd6f97a
ZigZag557 <bek_2001@hotmail.com>
2021-05-02 23:35:52 +0300

    Added a simple drawing functionality.

---
+++ Assets/DrawComponent.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawComponent : MonoBehaviour
+{
+
+    public int pixelAmount;
+    public Tool toolOnUse = Tool.Pen;
+    public Color pickedColor = Color.red;
+    public GameObject PixelPrefab;
+    bool isMouseOverBoard;
+
+
+    Vector2 lastMousePos = Vector2.zero;
+
+    private void Start()
+    {
+        for (int i = 0; i < pixelAmount; i++)
+        {
+            var pixel = Instantiate(PixelPrefab, transform);
+            pixel.GetComponent<Pixel>().pos = i;
+        }
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
+        {
+            if(lastMousePos == Vector2.zero)
+            {
+                lastMousePos = Input.mousePosition;
+                return;
+            }
+
+            Vector2 curMousePos = Input.mousePosition;
+
+            RaycastHit2D[] rays = Physics2D.LinecastAll(lastMousePos, curMousePos);
+            foreach (var ray in rays)
+            {
+                if(ray.collider != null && ray.collider.tag == "Pixel")
+                {
+
+                    ray.collider.GetComponent<Image>().color = Input.GetMouseButton(0) ? pickedColor : Color.white;
+                }
+            }
+
+            lastMousePos = curMousePos;
+        }
+        else
+        {
+            lastMousePos = Vector2.zero;
+        }
+    }
+
+    public void CleanUp()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).GetComponent<Image>().color = Color.white;
+        }
+    }
+
+    public void ChangeColor(string clr)
+    {
+        // Color code: RRR_GGG_BBB
+        string[] colors = clr.Split('_');
+        Color newClr = new Color(int.Parse(colors[0]), int.Parse(colors[1]), int.Parse(colors[2]));
+        pickedColor = newClr;
+    }
+    
+
+}
+
+public enum Tool
+{
+    Pen = 0,
+    Eraser = 1
+}
---
+++ Assets/Pixel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pixel : MonoBehaviour
+{
+    public int pos;
+    public Color clr;
+    public Sprite highlightImg;
+    Image selfImg;
+    bool isPointerInside;
+
+    private void Start()
+    {
+        selfImg = GetComponent<Image>();
+    }
+    public void PointerEnter() {
+        isPointerInside = true;
+        selfImg.sprite = highlightImg;
+   }
+
+    public void PointerExit() {
+        isPointerInside = false;
+        selfImg.sprite = null;
+     }
+}
